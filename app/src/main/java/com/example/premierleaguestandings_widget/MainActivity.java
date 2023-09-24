3b1268e9ed61656f842099da38b66861d6bd8258
913_Stefana_Hutupasu <stefana.hutupasu@gmail.com>
2023-09-24 06:53:18 +0300

    final

--- app/src/main/java/com/example/premierleaguestandings_widget/MainActivity.java
+++ app/src/main/java/com/example/premierleaguestandings_widget/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.premierleaguestandings_widget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        if (activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
+            // You have internet connectivity, proceed with making network requests
+            Toast.makeText(this, "Yes internet connection", Toast.LENGTH_SHORT).show();
+        } else {
+            // You don't have internet connectivity, show a message to the user
+            Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
